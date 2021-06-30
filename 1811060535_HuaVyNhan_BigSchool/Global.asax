efe8e876c83987422457e5e2201257fbc1a1cf6e
nhanhua0 <nhanhua0@gmail.com>
2021-06-30 13:47:40 +0700

    Add project files.

---
+++ 1811060535_HuaVyNhan_BigSchool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1811060535_HuaVyNhan_BigSchool.MvcApplication" Language="C#" %>
---
+++ 1811060535_HuaVyNhan_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811060535_HuaVyNhan_BigSchool.Startup))]
+namespace _1811060535_HuaVyNhan_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
